ed94ded7867100d22596260fb44408e5ecd62035
factorion <40322086+factorion@users.noreply.github.com>
2020-09-03 00:36:15 -0400

    Create first image (#24)
    Fixes #23
    Basic rendering, needs a lot of work

--- cmd/graytracer/main.go
+++ cmd/graytracer/main.go
@@ -2,20 +2,50 @@ package main
 
 import (
 	"fmt"
+	"image"
+	"image/color"
+	"image/png"
+	_ "math"
+	"os"
 	"github.com/factorion/graytracer/pkg/primitives"
+	"github.com/factorion/graytracer/pkg/shapes"
 )
 
 func main() {
-	vector := primitives.MakeVector(1.0, 0.0, 0.0)
-	fmt.Printf("Vector = %v\n", vector)
-	point := primitives.MakePoint(2.0, 3.0, 4.0)
-	fmt.Printf("Point = %v\n", point)
-	m := primitives.MakeMatrix(4)
-	fmt.Printf("Matrix = %v\n", m)
-	fmt.Println(len(m))
-	m2 := primitives.MakeMatrix(3)
-	fmt.Printf("Matrix3x3 = %v\n", m2)
-	fmt.Println(len(m2))
-	im := primitives.MakeIdentityMatrix(4)
-	fmt.Printf("Identity Matrix 2x2 = %v\n", im)
+	fmt.Println("Starting render")
+	width := 100
+	height := 100
+	upLeft := image.Point{0, 0}
+	lowRight := image.Point{width, height}
+	img := image.NewRGBA(image.Rectangle{upLeft, lowRight})
+	// sphere := shapes.MakeSphere(0, 0, 0, 1)
+	transform := primitives.Shearing(1, 0, 0, 0, 0, 0)
+	transform = transform.Multiply(primitives.Scaling(0.5, 1, 1))
+	sphere := shapes.MakeTransformedSphere(0, 0, 0, 1, transform)
+	wallZ := 10.0
+	wallSize := 7.0
+	canvasPixels := 100.0
+	pixelSize := wallSize / canvasPixels
+	half := wallSize / 2
+	origin := primitives.MakePoint(0, 0, -5)
+	for y := 0; y < height; y++ {
+		worldY := half - (pixelSize * float64(y))
+		for x := 0; x < width; x++ {
+			worldX := -half + (pixelSize * float64(x))
+			position := primitives.MakePoint(worldX, worldY, wallZ)
+			r := primitives.Ray{Origin:origin, Direction:position.Subtract(origin).Normalize()}
+			intersections := shapes.Intersections{}
+			hits := sphere.Intersect(r)
+			for i := range hits {
+				intersections[hits[i]] = shapes.Intersection{Distance:hits[i], Obj:sphere}
+			}
+			if len(intersections) != 0 {
+				img.Set(x, height - (y + 1), color.White)
+			} else {
+				img.Set(x, height - (y + 1), color.Black)
+			}
+		}
+	}
+	f, _ := os.Create("image.png")
+	png.Encode(f, img)
 }
--- pkg/shapes/intersection.go
+++ pkg/shapes/intersection.go
@@ -1,12 +1,30 @@
 package shapes
 
+import (
+	"math"
+)
+
+// SliceEquals Check if two slices are equal
+func SliceEquals(a, b []float64) bool {
+	if len(a) != len(b) {
+        return false
+	}
+	EPSILON := 0.00000001
+    for i, v := range a {
+        if math.Abs(v - b[i]) > EPSILON {
+            return false
+        }
+    }
+    return true
+}
+
 // Intersections Map of intersections keyed to their distance
 type Intersections map[float64]Intersection
 
 // Intersection Structure to hold intersection information
 type Intersection struct {
-	distance float64
-	shape Shape
+	Distance float64
+	Obj Shape
 }
 
 // Hit Get the closest hit from intersections
