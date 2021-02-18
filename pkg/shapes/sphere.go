6115a46df74c44bdc0d4ce0fbcb69200c875d74f
factorion <factorion@gmail.com>
2021-02-18 01:32:22 -0500

    Cylinders Added cylinders, edited CI and README

--- pkg/shapes/sphere.go
+++ pkg/shapes/sphere.go
@@ -20,11 +20,11 @@ func (s *Sphere) Intersect(r primitives.Ray) []float64 {
 	hits := []float64{}
 	// convert ray to object space
 	inverse, _ := s.transform.Inverse()
-	ray2 := r.Transform(inverse)
+	oray := r.Transform(inverse)
 	// Vector from the sphere's center
-	sray := ray2.Origin.Subtract(primitives.MakePoint(0, 0, 0))
-	a := ray2.Direction.DotProduct(ray2.Direction)
-	b := 2 * ray2.Direction.DotProduct(sray)
+	sray := oray.Origin.Subtract(primitives.MakePoint(0, 0, 0))
+	a := oray.Direction.DotProduct(oray.Direction)
+	b := 2 * oray.Direction.DotProduct(sray)
 	c := sray.DotProduct(sray) - 1
 	discriminant := (b * b) - (4 * a * c)
 	if discriminant < 0 {
